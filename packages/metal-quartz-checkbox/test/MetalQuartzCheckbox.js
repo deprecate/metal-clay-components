61c7d1b25cafcc863c0717dab9f5e3d9a7a22c20
Patrick Yeo <patrick.yeo@liferay.com>
2017-03-31 17:49:28 -0700

    Metal Quartz Checkbox: Tests

--- packages/metal-quartz-checkbox/test/MetalQuartzCheckbox.js
+++ packages/metal-quartz-checkbox/test/MetalQuartzCheckbox.js
@@ -12,7 +12,38 @@ describe('MetalQuartzCheckbox', function() {
 		}
 	});
 
-	it('should be tested', () => {
-		assert.fail();
+	it('should render the correct markup for a single checkbox', () => {
+		component = new MetalQuartzCheckbox({
+			checked: true,
+			disabled: true,
+			elementClasses: 'checkbox-default',
+			label: 'Checkbox 1 Label',
+			name: 'myUniqueCheckboxName_!!11!'
+		});
+
+		assert.strictEqual(component.element.outerHTML, __html__['test/fixture/testDefaultMetalQuartzCheckbox.html'].trim());
+	});
+
+	it('shound render the correct markup for an inline checkbox', () => {
+		component = new MetalQuartzCheckbox({
+			checkboxInline: [{
+				checked: true,
+				label: 'Inline 1'
+			}, {
+				label: 'Inline 2'
+			}, {
+				checked: true,
+				disabled: true,
+				label: 'Inline 3'
+			}, {
+				label: 'Inline 4'
+			}, {
+				label: 'Inline 5'
+			}, {
+				label: 'Inline 6'
+			}]
+		});
+
+		assert.strictEqual(component.element.outerHTML, __html__['test/fixture/testDefaultMetalQuartzCheckboxInline.html'].trim());
 	});
 });
