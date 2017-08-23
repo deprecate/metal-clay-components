8375d7a46ffc65ad36f3f538df3c27f8cd2a9c74
Matuzalém S. Teles <matuzalemteles@gmail.com>
2017-08-22 22:12:15 -0300

    Create Clay Badge component

---
+++ packages/clay-badge/src/ClayBadge.js
@@ -0,0 +1,128 @@
+'use strict';
+
+import 'clay-icon';
+import templates from './ClayBadge.soy.js';
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import { Config } from 'metal-state';
+
+/**
+ * Metal Clay Badge component.
+ */
+class ClayBadge extends Component {}
+
+/**
+ * State definition.
+ * @static
+ * #type {!Object}
+ */
+ClayBadge.STATE = {
+	/**
+	 * Render ClayIcon in the ClayBadge element.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?Object|undefined}
+	 * @default undefined
+	 */
+	icon: Config.shapeOf({
+		alignment: Config.oneOf(['left', 'right']),
+		monospaced: Config.bool(),
+		spritemap: Config.string().required(),
+		symbol: Config.string().required(),
+	}),
+
+	/**
+	 * Id to be applied to the element.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?string|undefined}
+	 * default undefined
+	 */
+	id: Config.string(),
+
+	/**
+	 * The label of the badge element.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?string|html|undefined}
+	 * default undefined
+	 */
+	label: Config.any(),
+
+	/**
+	 * The size of the badge element.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?string|undefined}
+	 * default undefined
+	 */
+	size: Config.oneOf(['sm', 'lg']),
+
+	/**
+	 * Shape of the badge.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?string|undefined}
+	 * default undefined
+	 */
+	shape: Config.oneOf(['pill']),
+
+	/**
+	 * HREF in all badge.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?string|undefined}
+	 * default undefined
+	 */
+	href: Config.string(),
+
+	/**
+	 * onClick in Icon of the Badge.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?func|undefined}
+	 * default undefined
+	 */
+	onClickIcon: Config.func(),
+
+	/**
+	 * onClick in Label of the Badge or in all Badge.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?func|undefined}
+	 * default undefined
+	 */
+	onClickLink: Config.func(),
+
+	/**
+	 * Option to enable href on every badge.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?func|undefined}
+	 * default undefined
+	 */
+	linkWrapper: Config.bool(),
+
+	/**
+	 * Badge style color.
+	 * @instance
+	 * @memberof ClayBadge
+	 * @type {?string|undefined}
+	 * default undefined
+	 */
+	style: Config.oneOf([
+		'primary',
+		'secondary',
+		'success',
+		'info',
+		'warning',
+		'danger',
+		'light',
+		'dark',
+	]).value('primary'),
+};
+
+Soy.register(ClayBadge, templates);
+
+export { ClayBadge };
+export default ClayBadge;
